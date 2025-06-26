ddc0bfaa20bbc8cdd171fb7f398ec4c58fa35721
OnurKerimoglu <kerimoglu.o@gmail.com>
2025-06-26 21:20:16 +0200

    04: streaming: fixed lambda_function

--- 04-deployment/streaming/lambda_function.py
+++ 04-deployment/streaming/lambda_function.py
@@ -36,35 +36,33 @@ def lambda_handler(event, context=None):
 
     prediction_events = []
     for record in event['Records']:
-       encoded_data = record['kinesis']['data']
-       decoded_data = base64.b64decode(encoded_data).decode('utf-8')
-       ride_event = json.loads(decoded_data)
-       print(f'ride event: {ride_event}')
-       ride = ride_event['ride']
-       ride_id = ride_event['ride_id']
-       features = prepare_features(ride)
-       prediction = predict(features)
-       prediction_event =  {
-        'model': 'ride_duration_prediction_model',
-        'version': '123',
-        'prediction': {
-            'ride_duration': prediction,
-            'ride_id': ride_id
-        }
-       }
-
-    if not TEST_RUN:
-       print('prod run, sending the prediction to kinesis')
-       response = kinesis_client.put_record(
-            StreamName=PREDICTION_STREAM_NAME,
-            Data=json.dumps(prediction_event),
-            PartitionKey=str(ride_id)
-        )
-       print(f'kinesis {PREDICTION_STREAM_NAME} put_record response:\n{response}')
-    else:
-        print('test run, not sending the prediction to kinesis')
-
-    prediction_events.append(prediction_event)
+        encoded_data = record['kinesis']['data']
+        decoded_data = base64.b64decode(encoded_data).decode('utf-8')
+        ride_event = json.loads(decoded_data)
+        print(f'ride event: {ride_event}')
+        ride = ride_event['ride']
+        ride_id = ride_event['ride_id']
+        features = prepare_features(ride)
+        prediction = predict(features)
+        prediction_event =  {
+            'model': 'ride_duration_prediction_model',
+            'version': '123',
+            'prediction': {
+                'ride_duration': prediction,
+                'ride_id': ride_id
+                }
+            }
+        if not TEST_RUN:
+            print('prod run, sending the prediction to kinesis')
+            response = kinesis_client.put_record(
+                StreamName=PREDICTION_STREAM_NAME,
+                Data=json.dumps(prediction_event),
+                PartitionKey=str(ride_id)
+            )
+            print(f'kinesis {PREDICTION_STREAM_NAME} put_record response:\n{response}')
+        else:
+            print('test run, not sending the prediction to kinesis')
+        
+        prediction_events.append(prediction_event)
     
-
     return {'prediction_events': prediction_events}
